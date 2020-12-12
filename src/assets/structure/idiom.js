02f7b0f5affa524198d4765d2a0928313e83bc26
zhouyuying <zhouyuying@chinaso.com>
2020-12-12 11:48:40 +0800

    提交一版消消乐

--- src/assets/structure/idiom.js
+++ src/assets/structure/idiom.js
@@ -4,6 +4,56 @@ const CARD_WIDTH = 50
 const CARD_HEIGHT = 50
 const CLOCK_WIDTH = 100
 const CLOCK_HEIGHT = 50
+/**
+ * 矩形和矩形的碰撞检测
+ * @param {*} r1
+ * @param {*} r2
+ */
+function hitTestRectangle(r1, r2) {
+  //Define the variables we'll need to calculate
+  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy
+
+  //hit will determine whether there's a collision
+  hit = false
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2
+  r1.centerY = r1.y + r1.height / 2
+  r2.centerX = r2.x + r2.width / 2
+  r2.centerY = r2.y + r2.height / 2
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2
+  r1.halfHeight = r1.height / 2
+  r2.halfWidth = r2.width / 2
+  r2.halfHeight = r2.height / 2
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX
+  vy = r1.centerY - r2.centerY
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+    //A collision might be occurring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+      //There's definitely a collision happening
+      hit = true
+    } else {
+      //There's no collision on the y axis
+      hit = false
+    }
+  } else {
+    //There's no collision on the x axis
+    hit = false
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit
+}
 const arrayShuffle = (arr, num = 1) => {
   const randomNumber = function() {
     // randomNumber(a,b) 返回的值大于 0 ，则 b 在 a 的前边；
